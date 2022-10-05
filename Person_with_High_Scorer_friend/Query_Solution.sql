c63db31e5b4dc872f6ec4bd47bd888603751baaf
Ankit Banerjee <37772760+Ankit1032@users.noreply.github.com>
2022-10-05 13:58:16 +0530

    Update Query_Solution.sql

--- Person_with_High_Scorer_friend/Query_Solution.sql
+++ Person_with_High_Scorer_friend/Query_Solution.sql
@@ -23,3 +23,20 @@ PERSONID, TOTAL_FRIENDS_SCORE, NO_OF_FRIENDS, PERSON_NAME, SCORE_OF_PERSON
 2	        115	                 2	            Bob	         11
 4	        101	                 3	            Tara	       45
 */
+
+--solution
+/* write a query to find personid, name, number of friends
+,sum of marks of the person who have friends with total score greater than 100
+*/
+with temp1 as (
+select p.*, f.friendid from person p
+inner join friend f
+on p.personid = f.personid
+),temp2 as (
+select t1.*,p.name as friend_name,p.score as friend_score from temp1 t1
+inner join person p
+on t1.friendid = p.personid)
+select name,sum(friend_score) from temp2
+group by name
+having sum(friend_score) > 100
+;
